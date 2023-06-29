1c0ed803afbcadbddb7becb05ca30af17c54cf22
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-06-29 15:15:02 +0200

    create base for my app with expo

---
+++ Navigation.js
@@ -0,0 +1,45 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Feed from './screens/Feed';
+import Settings from './screens/tabScreens/Settings';
+import Calendar from './screens/tabScreens/Calendar';
+
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+//Tab bottom
+const Tab = createBottomTabNavigator();
+
+function TabGroup() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route, navigation }) => ({
+                tabBarIcon: ({color, focused, size }) => {
+                    let iconName;
+                    let IconComponent = Ionicons;
+                    if (route.name === "Notes") {
+                        iconName = focused ? "notebook" : "notebook-outline";
+                        IconComponent = MaterialCommunityIcons;
+                    } else if (route.name === "Calendar") {
+                        iconName = focused ? "ios-calendar" : "ios-calendar-outline";
+                    } else if (route.name === "Settings") {
+                        iconName = focused ? "ios-settings-sharp" : "ios-settings-outline";
+                    }
+                    return <IconComponent name={iconName} size={size} color={color}/>
+                },
+                tabBarActiveTintColor: "#000000",
+        })}
+        >
+            <Tab.Screen name='Notes' component={Feed}/>
+            <Tab.Screen name='Calendar' component={Calendar}/>
+            <Tab.Screen name='Settings' component={Settings}/>
+        </Tab.Navigator>
+    )
+}
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <TabGroup/>
+        </NavigationContainer>
+    )
+}
